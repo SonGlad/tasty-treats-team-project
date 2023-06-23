0cc02b7ff9d4740c768c8bad9ebc837e25f6ad9e
SonGlad <121556627+SonGlad@users.noreply.github.com>
2023-06-23 17:33:02 +0000

    Deploying to gh-pages from @ SonGlad/tasty-treats-team-project@389caa32f29e98f16d4f1fb1c9d432d7dd0f98ae 🚀

---
+++ index.758e51a1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".mob-menu-btn"),n=document.querySelector(".mob-menu-btn-close"),t=document.querySelector(".mobile-menu");e.addEventListener("click",(function(){t.classList.toggle("open")})),n.addEventListener("click",(function(){t.classList.remove("open")}));
+//# sourceMappingURL=index.758e51a1.js.map
---
+++ index.79e15934.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".mob-menu-btn"),n=document.querySelector(".mob-menu-btn-close"),t=document.querySelector(".mobile-menu");e.addEventListener("click",(function(){t.classList.toggle("open")})),n.addEventListener("click",(function(){t.classList.remove("open")}))}();
+//# sourceMappingURL=index.79e15934.js.map
