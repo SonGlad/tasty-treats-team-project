4f625257774fcac74b9e2a951b914e32cf4bd642
SonGlad <121556627+SonGlad@users.noreply.github.com>
2023-06-23 18:08:22 +0000

    Deploying to gh-pages from @ SonGlad/tasty-treats-team-project@680b12149854fc12ea718ea5437c9bc682c3700d 🚀

--- index.758e51a1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".mob-menu-btn"),n=document.querySelector(".mob-menu-btn-close"),t=document.querySelector(".mobile-menu");e.addEventListener("click",(function(){t.classList.toggle("open")})),n.addEventListener("click",(function(){t.classList.remove("open")}));
-//# sourceMappingURL=index.758e51a1.js.map
