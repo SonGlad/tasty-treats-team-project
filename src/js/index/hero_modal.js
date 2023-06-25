f3485d9cc993c177b3d7b29db047b1abcddaae44
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-06-25 21:44:13 +0300

    validation, submit, hover

--- src/js/index/hero_modal.js
+++ src/js/index/hero_modal.js
@@ -1,63 +1,7 @@
+const modalOpenClose = require('../utils/modalOpenClose').default;
+const validateInput = require('../utils/validation').default;
 
-  const modal = document.querySelector('.modal');
-  const btn = document.querySelectorAll('.order-btn');
-  const closeBtn = document.getElementsByClassName('close')[0];
-  const form = document.getElementById('order-form');
 
-document.addEventListener('DOMContentLoaded', function() {
-  for (let i = 0; i < btn.length; i++) {
-    btn[i].onclick = function() {
-      modal.style.display = 'block';
-      document.body.style.overflow = "hidden";
-    };
-  }
+modalOpenClose();
+validateInput();
 
-  closeBtn.onclick = function() {
-    closeModal();
-  };
-
-  window.onclick = function(event) {
-    if (event.target === modal) {
-      closeModal();
-    }
-  };
-
-  window.addEventListener('keydown', function(event) {
-    if (event.key === 'Escape') {
-      closeModal();
-    }
-  });
-
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    if (validateForm()) {
-      console.log('API_POST_Request');
-    }
-  });
-
-  function closeModal() {
-    modal.style.display = 'none';
-    document.body.style.overflow = "";
-  }
-
-  function validateForm() {
-    const nameInput = document.getElementById('name');
-    const phoneInput = document.getElementById('phone');
-    const emailInput = document.getElementById('email');
-
-    const namePattern = /[A-Za-zА-Яа-яЁёІіЇїЄє' ]+/;
-    const phonePattern = /[0-9]{10}/;
-
-    if (!namePattern.test(nameInput.value)) {
-      alert('Enter a valid name');
-      return false;
-    }
-
-    if (!phonePattern.test(phoneInput.value)) {
-      alert('Enter a valid phone number');
-      return false;
-    }
-
-    return true;
-  }
-});
---
+++ src/js/utils/validation.js
@@ -0,0 +1,80 @@
+const validateInput = () => {
+  const inputElement = document.querySelector('.name-input');
+  const inputPhone = document.querySelector('.input-phone');
+  const inputEmail = document.querySelector('.email-input');
+  const inputComment = document.querySelector('.order-form-comment');
+  const form = document.querySelector('#order-form');
+
+
+ inputElement.addEventListener('input', (event) => {
+  validateField(inputElement, /^[A-Za-zА-Яа-яЁёІіЇїЄє\s]+$/);
+});
+
+  inputElement.addEventListener('blur', (event) => {
+    resetField(inputElement);
+  });
+
+  inputPhone.addEventListener('input', (event) => {
+    validateField(inputPhone, /^\d+$/);
+  });
+
+  inputPhone.addEventListener('blur', (event) => {
+    resetField(inputPhone);
+  });
+
+  inputEmail.addEventListener('input', (event) => {
+    validateField(inputEmail, /^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+  });
+
+  inputEmail.addEventListener('blur', (event) => {
+    resetField(inputEmail);
+  });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const nameValue = inputElement.value.trim();
+    const phoneValue = inputPhone.value.trim();
+    const emailValue = inputEmail.value.trim();
+    const commentValue = inputComment.value.trim();
+
+    if (validateField(inputElement, /^[A-Za-zА-Яа-яЁёІіЇїЄє\s]+$/) &&
+        validateField(inputPhone, /^\d+$/) &&
+        validateField(inputEmail, /^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      console.log(`https://tasty-treats-backend.p.goit.global/api/orders/add&{
+        name: ${nameValue},
+        phone: ${phoneValue},
+        email: ${emailValue},
+        comment:${commentValue},
+      }`);
+      resetField(inputPhone);
+      resetField(inputEmail);
+      resetField(inputElement);
+      inputElement.value = '';
+      inputPhone.value = '';
+      inputEmail.value = '';
+      inputComment.value = '';
+
+    }
+
+  });
+
+  function validateField(input, pattern) {
+    const value = input.value.trim();
+    const isValid = pattern.test(value);
+
+    if (isValid) {
+      input.style.border = '2px solid green';
+    } else {
+      input.style.border = '2px solid red';
+    }
+
+    return isValid;
+  }
+
+  function resetField(input) {
+    input.style.border = '1px solid var(--gray-title-w)';
+  }
+};
+
+export default validateInput;
