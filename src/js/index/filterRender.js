f2617c5f028c78f63ffde477824c39b3a4097768
maryan <smoliymaryan89@gmail.com>
2023-06-28 15:46:21 +0300

    fixes-all-categories

--- src/js/index/filterRender.js
+++ src/js/index/filterRender.js
@@ -178,3 +178,11 @@ function hide() {
   refs.pagination.classList.add('hide');
   refs.pagination.classList.remove('show');
 }
+
+document
+  .querySelector('.category-list')
+  .addEventListener('click', function (event) {
+    if (!event.target.classList.contains('category-btn')) {
+      event.stopPropagation();
+    }
+  });
