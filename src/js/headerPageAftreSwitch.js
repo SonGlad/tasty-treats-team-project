48940d62b48340d35fa34514e5b8a44261db30c3
SonGlad <okoshevy@gmail.com>
2023-06-30 01:07:33 +0300

    header nav link color fix

--- src/js/headerPageAftreSwitch.js
+++ src/js/headerPageAftreSwitch.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     menuItems.forEach(item => {
       item.classList.remove('current');
+      item.classList.remove('nav-link-active');
     });
   
 
     menuItems.forEach(item => {
       if (item.getAttribute('href') === currentPath) {
         item.classList.add('current');
+        item.classList.add('nav-link-active');
       }
     });
 });
