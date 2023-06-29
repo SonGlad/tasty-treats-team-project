e45e1883f6855aadd7b9949fbd561eaca6682606
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-06-29 18:41:50 +0300

    SmallFix

--- src/js/modalRating.js
+++ src/js/modalRating.js
@@ -1,56 +1,58 @@
 function modalRating() {
-    const refs = {
-        closeBtnModal: document.querySelector('.btn-close-rating'),
-        ratingBackdrop: document.querySelector('.rating-backdrop'),
-        ratingEmailBtn: document.querySelector('.rating-email-btn'),
+  const refs = {
+    closeBtnModal: document.querySelector('.btn-close-rating'),
+    ratingBackdrop: document.querySelector('.rating-backdrop'),
+    ratingEmailBtn: document.querySelector('.rating-email-btn'),
+    body: document.querySelector('.body'),
+  };
+
+  refs.closeBtnModal.addEventListener('click', () => {
+    refs.ratingBackdrop.classList.add('visible');
+    refs.body.classList.remove('scroll-lock');
+    changeColor(0);
+  });
+
+  refs.ratingEmailBtn.addEventListener('click', () => {
+    refs.ratingBackdrop.classList.add('visible');
+    refs.body.classList.remove('scroll-lock');
+    changeColor(0); // !!!!!!!!!!!!!!!!! при отправке на backend должы обновиться звезды, но и отправиться
+  });
+
+  refs.ratingBackdrop.addEventListener('click', evt => {
+    if (evt.target === refs.ratingBackdrop) {
+      refs.ratingBackdrop.classList.add('visible');
+      refs.body.classList.remove('scroll-lock');
     }
+  });
 
-    refs.closeBtnModal.addEventListener('click', () => {
-        refs.ratingBackdrop.classList.add('visible');
-        changeColor(0);
+  document.addEventListener('keydown', evt => {
+    if (evt.key === 'Escape') {
+      changeColor(0);
+      refs.ratingBackdrop.classList.add('visible');
+    }
+  });
+
+  const stars = document.querySelectorAll('.rating-star input[type="radio"]');
+  stars.forEach(star => {
+    star.addEventListener('click', () => {
+      const starCount = parseInt(star.value);
+      changeColor(starCount);
     });
-
-    refs.ratingEmailBtn.addEventListener('click', () => {
-        refs.ratingBackdrop.classList.add('visible');
-
-        changeColor(0); // !!!!!!!!!!!!!!!!! при отправке на backend должы обновиться звезды, но и отправиться
-    
-    });
-
-    refs.ratingBackdrop.addEventListener('click', (evt) => {
-        if (evt.target === refs.ratingBackdrop) {
-            refs.ratingBackdrop.classList.add('visible');
-        }
-    });
-
-    document.addEventListener('keydown', (evt) => {
-        if (evt.key === 'Escape') {
-            changeColor(0);
-            refs.ratingBackdrop.classList.add('visible');
-        }
-    });
-
-    const stars = document.querySelectorAll('.rating-star input[type="radio"]');
-    stars.forEach(star => {
-        star.addEventListener('click', () => {
-            const starCount = parseInt(star.value);
-            changeColor(starCount);
-        });
-    });
+  });
 }
 
 function changeColor(starCount) {
-    const stars = document.querySelectorAll('.rating-star input[type="radio"]');
+  const stars = document.querySelectorAll('.rating-star input[type="radio"]');
 
-    for (let i = 0; i < stars.length; i++) {
-        const starLabel = stars[i].nextElementSibling;
-        const starSVG = starLabel.querySelector('.star-rating');
-        if (i < starCount) {
-            starSVG.classList.add('active');
-        } else {
-            starSVG.classList.remove('active');
-        }
+  for (let i = 0; i < stars.length; i++) {
+    const starLabel = stars[i].nextElementSibling;
+    const starSVG = starLabel.querySelector('.star-rating');
+    if (i < starCount) {
+      starSVG.classList.add('active');
+    } else {
+      starSVG.classList.remove('active');
     }
+  }
 }
 
 modalRating();
