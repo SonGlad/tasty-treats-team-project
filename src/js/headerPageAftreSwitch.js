2c547054b6f1b1365fd3215ad6b0feadc4a9bf5d
SonGlad <okoshevy@gmail.com>
2023-06-30 01:31:46 +0300

    mob menu burger fix

--- src/js/headerPageAftreSwitch.js
+++ src/js/headerPageAftreSwitch.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const currentPath = window.location.pathname;
     const menuItems = document.querySelectorAll('.navigation a');
+    const mobMenuItems = document.querySelectorAll('.header-list-item')
   
 
     menuItems.forEach(item => {
       item.classList.remove('current');
       item.classList.remove('nav-link-active');
+    });
+
+    mobMenuItems.forEach(item => {
+      item.classList.remove('active-header-list-item');
     });
   
 
@@ -13,6 +18,12 @@ document.addEventListener('DOMContentLoaded', function() {
       if (item.getAttribute('href') === currentPath) {
         item.classList.add('current');
         item.classList.add('nav-link-active');
+      }
+    });
+
+    mobMenuItems.forEach(item => {
+      if (item.getAttribute('href') === currentPath) {
+        item.classList.add('active-header-list-item');
       }
     });
 });
