9a83097d28d5374bdbe7d2d854ebf28bc47109b4
fufushka <dimaukropdred@gmail.com>
2023-06-29 23:34:26 +0300

    a

--- src/favorites.js
+++ src/favorites.js
@@ -1,13 +1,15 @@
-import "./js/changeTheme"
+import './js/changeTheme';
 
 import './js/headerPageAftreSwitch';
 
 import './js/scrollToTop';
 
-import "./js/mobMenu"
+import './js/mobMenu';
 
-import "./js/favorite/hero_modal"
+import './js/favorite/hero_modal';
 
-import "./js/favorite/favorites_main"
+import './js/favorite/favorites_main';
 
 import './js/modalRating';
+
+import './js/TeamModalOpCl';
